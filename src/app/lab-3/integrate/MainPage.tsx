ac0e4fcf802708a6cbbdcf979f4734c9f5619cf8
Crewch <artemkr2003@mail.ru>
2024-05-11 22:38:09 +0300

    +

--- src/app/lab-3/integrate/MainPage.tsx
+++ src/app/lab-3/integrate/MainPage.tsx
@@ -1,7 +1,152 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Task5 } from '@/tests/lab3/integrate.test'
+import { Lexer, Parser } from '@/tests/lab3/lib'
+import { useEffect, useState } from 'react'
+
 const MainPage = () => {
-	return <div className=''></div>
+	const [n, setN] = useState(2)
+	const [x, setX] = useState<string[]>(['0', '0'])
+	const [y, setY] = useState('')
+	const [h, setH] = useState({ h1: 0, h2: 0 })
+	const [answer, setAnswer] = useState<string | null>(null)
+
+	const handleSolve = () => {
+		const lexer = new Lexer()
+		const parser = new Parser()
+		let fX = lexer.run(y)
+		fX = parser.toPostfix(fX)
+		let t5 = new Task5(x.map(Number), fX)
+		setAnswer(t5.run(h.h1, h.h2))
+	}
+
+	const handleTest = () => {
+		const testX = ['0', '2']
+		const testY = 'x/(x*x+9)'
+		const testH = { h1: 0.5, h2: 0.25 }
+
+		setN(2)
+		setX(testX)
+		setY(testY)
+		setH(testH)
+
+		const lexer = new Lexer()
+		const parser = new Parser()
+		let fX = lexer.run(testY)
+		fX = parser.toPostfix(fX)
+		let t5 = new Task5(testX.map(Number), fX)
+		setAnswer(t5.run(testH.h1, testH.h2))
+	}
+
+	const handleClear = () => {
+		setAnswer(null)
+		setN(2)
+		setX(['0', '0'])
+	}
+
+	const updateVectorX = (index: number, value: string) => {
+		const newVector = [...x]
+		newVector[index] = value
+		setX(newVector)
+	}
+
+	useEffect(() => {
+		const updateVectorSize = (newSize: number) => {
+			let newVectorX = [...x]
+			const vectorLen = newVectorX.length
+
+			if (newSize > vectorLen) {
+				for (let i = vectorLen; i < newSize; i++) {
+					newVectorX[i] = '0'
+				}
+			} else {
+				newVectorX = newVectorX.slice(0, newSize)
+			}
+
+			setX(newVectorX)
+		}
+
+		updateVectorSize(n)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [n])
+
+	return (
+		<div>
+			<div className='flex justify-center gap-1 my-5'>
+				<Button onClick={handleSolve}>Solve</Button>
+				<Button onClick={handleTest}>Test</Button>
+				<Button onClick={handleClear}>Clear</Button>
+			</div>
+			<section className='flex justify-center gap-5 my-5'>
+				<div>
+					<Label>n</Label>
+					<Input
+						type='number'
+						value={n}
+						onChange={e => setN(+e.target.value)}
+						onKeyDown={e => {
+							if (e.key !== 'ArrowUp' && e.key !== 'ArrowDown')
+								e.preventDefault()
+						}}
+						className='w-20'
+						min={2}
+						max={10}
+					/>
+				</div>
+				<div className='flex flex-col gap-3'>
+					<div>
+						<Label>x</Label>
+						<div className='flex gap-1'>
+							{x.map((item, index) => (
+								<div key={index}>
+									<Input
+										type='number'
+										value={item}
+										onChange={e => updateVectorX(index, e.target.value)}
+										className='w-28'
+									/>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+				<div>
+					<Label>y</Label>
+					<Input value={y} onChange={e => setY(e.target.value)} />
+				</div>
+				<div>
+					<Label>h1</Label>
+					<Input
+						type='number'
+						value={h.h1}
+						onChange={e => setH({ ...h, h1: +e.target.value })}
+						step={0.1}
+						className='w-20'
+					/>
+				</div>
+				<div>
+					<Label>h2</Label>
+					<Input
+						type='number'
+						value={h.h2}
+						onChange={e => setH({ ...h, h2: +e.target.value })}
+						step={0.1}
+						className='w-20'
+					/>
+				</div>
+			</section>
+			<div className='flex justify-center'>
+				{answer && (
+					<div>
+						<pre>{answer}</pre>
+					</div>
+				)}
+			</div>
+		</div>
+	)
 }
 
 export default MainPage
---
+++ src/tests/lab3/integrate.test.ts
@@ -0,0 +1,158 @@
+import { Solver, Token } from './lib'
+
+export class Task5 {
+	private readonly X: number[]
+	private functionTokens: Token[]
+	private readonly solver: Solver
+
+	constructor(x: number[], functionTokens: Token[]) {
+		this.X = x
+		this.functionTokens = functionTokens
+		this.solver = new Solver()
+	}
+
+	private static str(value: number): string {
+		return value.toFixed(4)
+	}
+
+	private static printString(
+		i: string,
+		xi: string,
+		yi: string,
+		rect: string,
+		trp: string,
+		simp: string
+	): string {
+		const pad = 20
+		let res = ''
+		res += i.padEnd(pad)
+		res += xi.padEnd(pad)
+		res += yi.padEnd(pad)
+		res += rect.padEnd(pad)
+		res += trp.padEnd(pad)
+		res += simp.padEnd(pad)
+		res += '\n'
+		return res
+	}
+
+	public runge(
+		s1: number,
+		s2: number,
+		h1: number,
+		h2: number,
+		p: number
+	): number {
+		return s1 + (s1 - s2) / (Math.pow(h2 / h1, p) - 1)
+	}
+
+	public integrate(h: number): [string, number[][]] {
+		let res = `h = ${h}:\n\n`
+		let sum = 0
+		let X0 = this.X[0]
+
+		let range = Math.ceil((this.X[1] - this.X[0]) / h) + 1
+		let iter = 0
+		let table = Array.from(Array(range), () => new Array(5).fill(0))
+
+		while (X0 <= this.X[1]) {
+			table[iter][0] = X0
+			table[iter][1] = this.solver.solve(this.functionTokens, X0, 0, 0)
+			X0 += h
+			iter++
+		}
+		X0 = this.X[0]
+		iter = 0
+
+		while (X0 <= this.X[1]) {
+			table[iter][2] = sum
+			sum += this.solver.solve(this.functionTokens, (2 * X0 + h) / 2, 0, 0) * h
+			X0 += h
+			iter++
+		}
+		X0 = this.X[0]
+		iter = 0
+		sum = 0
+
+		while (X0 <= this.X[1]) {
+			table[iter][3] = sum / 2
+			sum +=
+				(this.solver.solve(this.functionTokens, X0 + h, 0, 0) +
+					this.solver.solve(this.functionTokens, X0, 0, 0)) *
+				h
+			X0 += h
+			iter++
+		}
+		X0 = this.X[0]
+		iter = 0
+		sum = 0
+
+		while (X0 <= this.X[1]) {
+			if (iter % 2 !== 0) {
+				iter++
+				continue
+			}
+			table[iter][4] = sum / 3
+			sum +=
+				(this.solver.solve(this.functionTokens, X0, 0, 0) +
+					4 * this.solver.solve(this.functionTokens, X0 + h, 0, 0) +
+					this.solver.solve(this.functionTokens, X0 + 2 * h, 0, 0)) *
+				h
+			X0 += 2 * h
+			iter++
+		}
+
+		res += Task5.printString(
+			'i',
+			'xi',
+			'yi',
+			'Rectangles',
+			'Trapezoids',
+			'Simpson'
+		)
+		for (let i = 0; i < range; i++) {
+			res += Task5.printString(
+				i.toString(),
+				Task5.str(table[i][0]),
+				Task5.str(table[i][1]),
+				Task5.str(table[i][2]),
+				Task5.str(table[i][3]),
+				Task5.str(table[i][4])
+			)
+		}
+		return [res, table]
+	}
+
+	public run(h1: number, h2: number): string {
+		let res = ''
+
+		const [res1, table1] = this.integrate(h1)
+		res += res1
+		const [res2, table2] = this.integrate(h2)
+		res += '\n' + res2 + '\n'
+		res += 'Runge method:\n'
+		const range1 = Math.ceil((this.X[1] - this.X[0]) / h1)
+		const range2 = Math.ceil((this.X[1] - this.X[0]) / h2)
+		res += `Rectangles: ${this.runge(
+			table1[range1][2],
+			table2[range2][2],
+			h1,
+			h2,
+			2
+		)}\n`
+		res += `Trapezoids: ${this.runge(
+			table1[range1][3],
+			table2[range2][3],
+			h1,
+			h2,
+			2
+		)}\n`
+		res += `Simpson: ${this.runge(
+			table1[range1][4],
+			table2[range2][4],
+			h1,
+			h2,
+			2
+		)}\n`
+		return res
+	}
+}
