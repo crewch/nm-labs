166e44a763f5b04174de7a515b62ebfac3685aff
Crewch <artemkr2003@mail.ru>
2024-06-01 20:52:15 +0300

    +

--- src/tests/lab1/qr-algorithm.test.ts
+++ src/tests/lab1/qr-algorithm.test.ts
@@ -13,22 +13,17 @@ export const qrTest = () => {
 	return { A, B }
 }
 
-// Функция для создания матрицы с заданными размерами и начальным значением
 function createMatrix(
 	rows: number,
 	cols: number,
 	defaultValue: number = 0
 ): IMatrix {
-	// Создаем матрицу заданного размера, заполненную defaultValue
 	return Array.from({ length: rows }, () => Array(cols).fill(defaultValue))
 }
 
-// Функция для умножения матриц
 function matrixMultiply(A: IMatrix, B: IMatrix): IMatrix {
-	// Инициализация результирующей матрицы нулями
 	let result = A.map(row => Array(B[0].length).fill(0))
 
-	// Тройной цикл для умножения матриц
 	for (let i = 0; i < A.length; i++) {
 		for (let j = 0; j < B[0].length; j++) {
 			for (let k = 0; k < A[0].length; k++) {
@@ -37,147 +32,105 @@ function matrixMultiply(A: IMatrix, B: IMatrix): IMatrix {
 		}
 	}
 
-	// Возвращаем результат
 	return result
 }
 
-// Функция для создания единичной матрицы заданного размера
 function identityMatrix(size: number): IMatrix {
-	// Создаем матрицу с заданным размером и нулевыми значениями
 	let I = createMatrix(size, size)
-	// Заполняем диагональ единицами
+
 	for (let i = 0; i < size; i++) {
 		I[i][i] = 1
 	}
-	// Возвращаем единичную матрицу
+
 	return I
 }
 
-// Функция для выполнения QR-разложения
 function QRDecomposition(A: IMatrix, epsilon: number): [IMatrix, IMatrix] {
-	// Получаем размерность матрицы
 	let n = A.length
-	// Инициализируем Q как единичную матрицу
 	let Q = identityMatrix(n)
-	// Копируем матрицу A в R
 	let R = A.map(row => [...row])
 
-	// Основной цикл для QR-разложения
 	for (let i = 0; i < n - 1; i++) {
-		// Инициализация вектора nu
 		let nu: IVector = Array(n).fill(0)
-		// Вычисление нормы
 		let norm = 0
 
-		// Вычисление нормы для текущего столбца R
 		for (let j = i; j < n; j++) {
 			norm += R[j][i] * R[j][i]
 		}
 
-		// Определение знака для nu[i]
 		let sign = R[i][i] < 0 ? -1 : 1
-		// Вычисление первого элемента вектора nu
 		nu[i] = R[i][i] + sign * Math.sqrt(norm)
 
-		// Вычисление остальных элементов вектора nu
 		for (let j = i + 1; j < n; j++) {
 			nu[j] = R[j][i]
 		}
 
-		// Инициализация матрицы Хаусхолдера H
 		let H = createMatrix(n, n)
-		// Коэффициент для нормализации вектора nu
 		let coef = 0
 
-		// Вычисление коэффициента
 		for (let j = 0; j < n; j++) {
 			coef += nu[j] * nu[j]
 		}
 
-		// Проверка коэффициента на близость к нулю
 		if (Math.abs(coef) < epsilon) {
 			throw new Error(
 				'Coefficient too close to zero, cannot proceed with QR Decomposition'
 			)
 		}
 
-		// Заполнение матрицы Хаусхолдера H
 		for (let j = 0; j < n; j++) {
 			for (let k = 0; k < n; k++) {
 				H[j][k] = (-2 * nu[j] * nu[k]) / coef
 			}
-			// Добавление единиц на диагонали матрицы H
 			H[j][j] += 1
 		}
 
-		// Обновление матрицы Q
 		Q = matrixMultiply(Q, H)
-		// Обновление матрицы R
 		R = matrixMultiply(H, R)
 	}
 
-	// Возвращаем матрицы Q и R
 	return [Q, R]
 }
 
-// Тип для представления комплексных чисел
 type Complex = [number, number]
 
-// Функция для вычитания комплексных чисел
 function subComplex([a, b]: Complex, [c, d]: Complex): Complex {
 	return [a - c, b - d]
 }
 
-// Функция для вычисления модуля комплексного числа
 function absComplex([a, b]: Complex): number {
 	return Math.sqrt(a * a + b * b)
 }
 
-// Функция для преобразования комплексного числа в строку
 function complexToString([real, imag]: Complex): string {
-	// Возвращаем строку в зависимости от наличия мнимой части
 	if (imag === 0) return real.toFixed(4)
 	return `${real.toFixed(4)} + ${imag.toFixed(4)}i`
 }
 
-// Функция для выполнения QR-алгоритма
 export function runQRAlgorithm(A: IMatrix, epsilon: number) {
-	// Получение размера матрицы
 	let n = A.length
-	// Инициализация массивов для текущих и предыдущих значений
 	let current: Complex[] = Array(n).fill([0, 0])
 	let prev: Complex[] = Array(n).fill([0, 0])
-	// Счетчик итераций
 	let iter = 0
-	// Флаг для определения сходимости
 	let flag: boolean
-	// Строка для хранения результатов
 	let res: (string | IMatrix | IVector | string[])[] = []
 
-	// Цикл, выполняющийся до сходимости
 	do {
-		// Получение матриц Q и R из QR-разложения
 		let [Q, R] = QRDecomposition(A, epsilon)
-		// Обновление матрицы A
 		A = matrixMultiply(R, Q)
 
-		// Добавление информации о текущей итерации в результат
 		res.push(`Iteration ${iter + 1}:`)
 		res.push('Matrix Q:', Q)
 		res.push('Matrix R:', R)
 		res.push('Updated Matrix A:', A)
 
-		// Вычисление собственных значений
 		for (let i = 0; i < n; i++) {
 			if (
-				// Условие для определения, является ли элемент диагональным
 				i === n - 1 ||
 				absComplex(subComplex([A[i + 1][i], 0], [0, 0])) < epsilon
 			) {
-				// Для диагональных элементов
 				current[i] = [A[i][i], 0]
 			} else {
-				// Для не диагональных элементов
 				let a = A[i][i]
 				let d = A[i + 1][i + 1]
 				let b = A[i][i + 1]
@@ -187,21 +140,17 @@ export function runQRAlgorithm(A: IMatrix, epsilon: number) {
 				let discriminant = tr * tr - 4 * det
 
 				if (discriminant < 0) {
-					// Для комплексных собственных значений
 					let realPart = tr / 2
 					let imaginaryPart = Math.sqrt(-discriminant) / 2
 					current[i] = [realPart, imaginaryPart]
 					current[i + 1] = [realPart, -imaginaryPart]
-					// Пропускаем следующий элемент, так как он является частью той же 2x2 подматрицы
 					i++
 				} else {
-					// Для действительных собственных значений, аналогично предыдущему случаю
 					current[i] = [A[i][i], 0]
 				}
 			}
 		}
 
-		// Проверка на сходимость
 		flag = true
 		for (let j = 0; j < current.length; j++) {
 			if (absComplex(subComplex(current[j], prev[j])) > epsilon) {
@@ -210,16 +159,12 @@ export function runQRAlgorithm(A: IMatrix, epsilon: number) {
 			}
 		}
 
-		// Обновление предыдущих значений
 		prev = current.map(x => [...x])
-		// Обнуление текущих значений
 		current.fill([0, 0])
 
-		// Увеличение счетчика итераций
 		iter++
 	} while (!flag)
 
-	// Добавление в результат списка собственных значений
 	res.push('Eigenvalues:')
 	const eigenvalueAns: string[] = []
 	for (const eigenvalue of prev) {
@@ -227,9 +172,7 @@ export function runQRAlgorithm(A: IMatrix, epsilon: number) {
 	}
 	res.push(eigenvalueAns)
 
-	// Добавление общего количества итераций в результат
 	res.push('Total Iterations:', `${iter}`)
-	// Включаем в строку `res` итоговые результаты и собственные значения
 
 	return res
 }
--- src/tests/lab1/rotation-method.test.ts
+++ src/tests/lab1/rotation-method.test.ts
@@ -14,28 +14,23 @@ export const rotationTest = () => {
 }
 
 function transpose(matrix: IMatrix): IMatrix {
-	// Функция для транспонирования матрицы
 	const rows = matrix.length
 	const cols = matrix[0].length
 	const transposed: IMatrix = Array.from(
 		{ length: cols },
 		() => new Array(rows)
 	)
-	// Создаем транспонированную матрицу
 
 	for (let i = 0; i < rows; i++) {
 		for (let j = 0; j < cols; j++) {
 			transposed[j][i] = matrix[i][j]
-			// Переставляем элементы матрицы
 		}
 	}
 
 	return transposed
-	// Возвращаем транспонированную матрицу
 }
 
 function multiplyMatrices(matrix1: IMatrix, matrix2: IMatrix): IMatrix {
-	// Функция для умножения матриц
 	const result: IMatrix = []
 
 	for (let i = 0; i < matrix1.length; i++) {
@@ -46,76 +41,58 @@ function multiplyMatrices(matrix1: IMatrix, matrix2: IMatrix): IMatrix {
 
 			for (let k = 0; k < matrix1[0].length; k++) {
 				sum += matrix1[i][k] * matrix2[k][j]
-				// Суммируем произведения элементов
 			}
 
 			result[i][j] = sum
-			// Присваиваем элементу результата сумму
 		}
 	}
 
 	return result
-	// Возвращаем результирующую матрицу
 }
 
 function identityMatrix(size: number): IMatrix {
-	// Функция для создания единичной матрицы
 	return Array.from({ length: size }, (_, i) =>
 		Array.from({ length: size }, (_, j) => (i === j ? 1 : 0))
 	)
-	// Заполняем матрицу, где диагональные элементы равны 1, остальные 0
 }
 
 function matrixEquals(matrix1: IMatrix, matrix2: IMatrix): boolean {
-	// Функция для проверки равенства двух матриц
 	for (let i = 0; i < matrix1.length; i++) {
 		for (let j = 0; j < matrix1[0].length; j++) {
 			if (matrix1[i][j] !== matrix2[i][j]) return false
-			// Если элементы не равны, возвращаем false
 		}
 	}
 
 	return true
-	// Если все элементы равны, возвращаем true
 }
 
 function calculateT(matrix: IMatrix): number {
-	// Функция для вычисления суммы квадратов недиагональных элементов матрицы
 	let res = 0
 
 	for (let i = 0; i < matrix.length - 1; i++) {
 		for (let j = i + 1; j < matrix[i].length; j++) {
 			res += matrix[i][j] * matrix[i][j]
-			// Складываем квадраты элементов
 		}
 	}
 
 	return Math.sqrt(res)
-	// Возвращаем квадратный корень из суммы квадратов
 }
 
 export function jacobiMethod(
 	A: IMatrix,
 	epsilon: number
 ): (IMatrix | IVector | string)[] {
-	// Функция метода Якоби для нахождения собственных значений и векторов матрицы
 	let res: (IMatrix | IVector | string)[] = []
-	// Массив для сохранения результатов
 
 	if (!matrixEquals(A, transpose(A))) {
 		return ['Invalid matrix']
-		// Проверяем, является ли матрица симметричной
 	}
 
 	const n = A.length
-	// Получаем размерность матрицы
 	let k = 0
-	// Инициализируем счетчик итераций
 	let globalU = identityMatrix(n)
-	// Создаем глобальную единичную матрицу U
 
 	while (calculateT(A) > epsilon) {
-		// Пока сумма квадратов недиагональных элементов больше epsilon
 		let maxVal = 0
 		let maxI = 0
 		let maxJ = 0
@@ -124,83 +101,60 @@ export function jacobiMethod(
 			for (let j = i + 1; j < n; j++) {
 				if (Math.abs(A[i][j]) > Math.abs(maxVal)) {
 					maxVal = A[i][j]
-					// Находим максимальный по модулю недиагональный элемент
 					maxI = i
 					maxJ = j
-					// Запоминаем его индексы
 				}
 			}
 		}
 
 		let U = identityMatrix(n)
-		// Создаем единичную матрицу U для этой итерации
 		let phiK
-		// Угол вращения
 
 		if (A[maxI][maxI] !== A[maxJ][maxJ]) {
 			phiK =
 				0.5 * Math.atan((2 * A[maxI][maxJ]) / (A[maxI][maxI] - A[maxJ][maxJ]))
-			// Вычисляем угол phiK
 		} else {
 			phiK = Math.PI / 4
-			// Если элементы диагонали равны, phiK равно π/4
 		}
 
 		U[maxI][maxJ] = -Math.sin(phiK)
 		U[maxJ][maxI] = Math.sin(phiK)
 		U[maxI][maxI] = Math.cos(phiK)
 		U[maxJ][maxJ] = Math.cos(phiK)
-		// Заполняем матрицу U
 
 		res.push(`Iteration ${k}: a[i,j]^(${k}) = ${maxVal}`)
-		// Добавляем информацию об итерации в результат
 		res.push(`Matrix U^${k}`)
-		// Добавляем матрицу U текущей итерации
 		res.push(U.map(row => row.map(value => +value.toFixed(4))))
-		// Добавляем матрицу U с округленными элементами
 
 		let uT = transpose(U)
-		// Транспонируем матрицу U
 		let nextA = multiplyMatrices(multiplyMatrices(uT, A), U)
-		// Вычисляем следующую матрицу A
 
 		for (let i = 0; i < n; i++) {
 			for (let j = 0; j < n; j++) {
 				A[i][j] = nextA[i][j]
-				// Обновляем матрицу A
 			}
 		}
 
 		k++
-		// Увеличиваем счетчик итераций
 		globalU = multiplyMatrices(globalU, U)
-		// Обновляем глобальную матрицу U
 
 		res.push(`Matrix A^${k}`)
-		// Добавляем обновленную матрицу A
 		res.push(A.map(row => row.map(value => +value.toFixed(4))))
-		// Добавляем матрицу A с округленными элементами
 		res.push(`t(A) after iteration ${k}: ${calculateT(A).toFixed(4)}`)
-		// Добавляем значение t(A) после итерации
 	}
 
 	res.push('Matrix U:')
 	res.push(globalU)
-	// Добавляем глобальную матрицу U
 	res.push('Eigenvalues:')
-	// Добавляем информацию о собственных значениях
 
 	const eigenvalues: IVector = []
 
 	for (let i = 0; i < n; i++) {
 		eigenvalues.push(+A[i][i].toFixed(4))
-		// Добавляем собственные значения
 	}
 	res.push(eigenvalues)
-	// Добавляем вектор собственных значений
 
 	res.push('Eigenvectors:')
-	// Добавляем информацию о собственных векторах
 
 	const eigenvectors: IMatrix = []
 
@@ -209,15 +163,12 @@ export function jacobiMethod(
 
 		for (let j = 0; j < n; j++) {
 			row.push(+globalU[j][i].toFixed(4))
-			// Добавляем элементы собственных векторов
 		}
 
 		eigenvectors.push(row)
 	}
 
 	res.push(eigenvectors)
-	// Добавляем матрицу собственных векторов
 
 	return res
-	// Возвращаем результаты метода Якоби
 }
